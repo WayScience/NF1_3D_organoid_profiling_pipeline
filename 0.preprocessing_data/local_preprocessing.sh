77fef6b7531f8ac82e9b29c0a33b7dd432eaf547
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2024-12-04 14:11:30 -0700

    inital commit + preprocessing (#1)
    * inital commit
    
    * update paths
    
    * preprocessing
    
    * run on alpine
    
    * change pathing
    
    * HPC
    
    * local update
    
    * addressing comments

--- 0.preprocessing_data/local_preprocessing.sh
+++ 0.preprocessing_data/local_preprocessing.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 conda activate gff_preprocessing_env
-git_root=$(git rev-parse --show-toplevel)
-if [ -z "$git_root" ]; then
-    echo "Error: Could not find the git root directory."
-    exit 1
-fi
 
-jupyter nbconvert --to=script --FilesWriter.build_directory="$git_root"/0.preprocessing_data/scripts/ "$git_root"/0.preprocessing_data/notebooks/*.ipynb
+jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*.ipynb
 
-python "$git_root"/0.preprocessing_data/scripts/0.patient_specific_preprocessing.py
-python "$git_root"/0.preprocessing_data/scripts/1.update_file_structure.py
-python "$git_root"/0.preprocessing_data/scripts/2.make_z-stack_images.py
+cd scripts/ || exit
 
+python 0.update_file_structure.py --HPC False
+python 1.make_z-stack_images.py
+
+cd .. || exit
 
 conda deactivate
 
 echo "Preprocessing complete"
-
