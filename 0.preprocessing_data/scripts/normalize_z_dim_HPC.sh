dfd95a903bcc71e84138dca9b2481a93cfcc5479
MikeLippincott <1michaell2017@gmail.com>
2024-12-22 13:48:55 -0700

    alpine run

--- 0.preprocessing_data/scripts/normalize_z_dim_HPC.sh
+++ 0.preprocessing_data/scripts/normalize_z_dim_HPC.sh
@@ -4,9 +4,9 @@
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
 #SBATCH --account=amc-general
-#SBATCH --time=00:60:00
-#SBATCH --output=preprocessing-%j.out
-#SBATCH --array=1-2
+#SBATCH --time=00:5:00
+#SBATCH --output=z-norm-%j.out
+#SBATCH --array=1-105
 
 
 module load anaconda
@@ -17,14 +17,18 @@ conda activate gff_preprocessing_env
 # get a list of all directories in the data/z-stack_images directory
 
 dirs_list=$(ls -d ../../data/z-stack_images/*)
+# make the list of directories into an array
+dirs_list=($dirs_list)
+n_sets=${#dirs_list[@]}
+echo $n_sets
 
 # index the list of directories
-job_id=$SLURM_ARRAY_TASK_ID
+job_id=$((SLURM_ARRAY_TASK_ID - 1))
 dir_idx=$((job_id % ${#dirs_list[@]}))
 
 dir=${dirs_list[$dir_idx]}
-
-python 2.z_slice_instensity_normalization.py --input_dir $dir
+echo "$dir"
+python 2.z_slice_instensity_normalization.py --input_dir "$dir"
 
 conda deactivate
 
