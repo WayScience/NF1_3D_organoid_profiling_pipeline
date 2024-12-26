fffdf32cf6a580b47a08ca2b59df339f282b3a0c
MikeLippincott <1michaell2017@gmail.com>
2024-12-26 09:32:18 -0700

    fix slurm script

--- 1.segment_images/HPC_segmentation.sh
+++ 1.segment_images/HPC_segmentation.sh
@@ -5,12 +5,12 @@
 #SBATCH --qos=normal
 #SBATCH --account=amc-general
 #SBATCH --time=30:00
-#SBATCH --output=child_output-%j.out
+#SBATCH --output=main_job_submit_output-%j.out
 
 cd slurm_scripts || exit
 
 job1=$(sbatch inital_semgentation_HPC.sh | awk '{print $4}')
 
-sbatch --dependency=afterok:$job1
+sbatch --dependency=afterok:$job1 process_segmentation_parent.sh
 
 exit 0
