8e478433ee6a3f840f8b9936641c57b93da63449
MikeLippincott <1michaell2017@gmail.com>
2024-12-30 13:38:23 -0700

    preprocessing data

--- 0.preprocessing_data/local_preprocessing.sh
+++ 0.preprocessing_data/local_preprocessing.sh
@@ -6,8 +6,15 @@ jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/ notebooks/*
 
 cd scripts/ || exit
 
-python 0.update_file_structure.py --HPC False
-python 1.make_z-stack_images.py
+# python 0.update_file_structure.py --HPC False
+# python 1.make_z-stack_images.py
+
+mapfile -t dirs < <(ls -d ../../data/z-stack_images/*)
+
+for dir in "${dirs[@]}"; do
+    echo "Processing directory: $dir"
+    python 2.z_slice_instensity_normalization.py --input_dir "$dir"
+done
 
 cd .. || exit
 
