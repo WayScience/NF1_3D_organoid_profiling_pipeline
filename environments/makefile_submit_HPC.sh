292639bb61d94322cd6bb880b08afb6854322381
MikeLippincott <1michaell2017@gmail.com>
2025-11-11 21:56:42 -0700

    Adding makefile for updating envs

--- environments/makefile_submit_HPC.sh
+++ environments/makefile_submit_HPC.sh
@@ -8,7 +8,16 @@
 #SBATCH --output=setup_envs-%j.out
 
 # Load your module for conda/mamba if needed
-module load miniforge
+module load minifonrge
+module load make
 
+INSTALL_OR_UPDATE="install-all"
 # Run Makefile
-make --always-make
+if [ "$INSTALL_OR_UPDATE" == "install-all" ]; then
+    make --install-all
+elif [ "$INSTALL_OR_UPDATE" == "update-all" ]; then
+    make --update-all
+else
+    echo "Invalid option for INSTALL_OR_UPDATE. Use 'install-all' or 'update-all'."
+    exit 1
+fi
