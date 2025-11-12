85458de15036651f87f32b621f7d29498e3cebd7
MikeLippincott <1michaell2017@gmail.com>
2025-11-11 20:01:14 -0700

    fixing segmentation

--- 2.segment_images/scripts/0.nuclei_segmentation.py
+++ 2.segment_images/scripts/0.nuclei_segmentation.py
@@ -9,13 +9,11 @@
 #
 # No we are at ~8 minutes!
 
-# In[ ]:
+# In[1]:
 
 
-import argparse
 import os
 import pathlib
-import sys
 import time
 
 import matplotlib.pyplot as plt
@@ -28,6 +26,8 @@ from arg_parsing_utils import check_for_missing_args, parse_args
 from cellpose import models
 from file_reading import read_zstack_image
 from notebook_init_utils import bandicoot_check, init_notebook
+from nuclei_segmentation import *
+from segmentation_decoupling import *
 from skimage.filters import sobel
 
 from segmentation_utils import *
@@ -50,7 +50,7 @@ image_base_dir = bandicoot_check(
 )
 
 
-# In[ ]:
+# In[4]:
 
 
 if not in_notebook:
@@ -70,9 +70,9 @@ if not in_notebook:
 else:
     print("Running in a notebook")
     patient = "NF0037_T1-Z-1"
-    well_fov = "F4-2"
+    well_fov = "F4-1"
     window_size = 3
-    clip_limit = 0.01
+    clip_limit = 0.03
     input_subparent_name = "zstack_images"
     mask_subparent_name = "segmentation_masks"
 
@@ -91,100 +91,15 @@ mask_path.mkdir(exist_ok=True, parents=True)
 
 return_dict = read_in_channels(find_files_available(input_dir))
 nuclei = return_dict["nuclei"]
-cyto2 = return_dict["cyto2"]
 # run clip_limit here
 nuclei = skimage.exposure.equalize_adapthist(
     nuclei, clip_limit=clip_limit, kernel_size=None
 )
-cyto2 = skimage.exposure.equalize_adapthist(
-    cyto2, clip_limit=clip_limit, kernel_size=None
-)
-
-
-# ## Organoid segmentation
-
-# In[ ]:
-
-
-cyto2_image_shape = cyto2.shape
-# commenting out optimization for time being
-# uncomment to run optimization
-# butterworth_grid_optimization(two_point_five_D_sliding_window, return_plot=False)
-
-filtered_cyto2 = apply_butterworth_filter(
-    sliding_window_two_point_five_D(cyto2, window_size=4),  # cyto
-    cutoff_frequency_ratio=0.05,
-    order=1,
-    high_pass=False,
-    squared_butterworth=True,
-)
-model = models.CellposeModel(
-    gpu=[True if torch.cuda.is_available() else False][0],
-    model_type="cyto3",  # CP3
-)
-output_dict = {
-    "slice": [],
-    "labels": [],
-    "details": [],
-}
-for slice in tqdm.tqdm(range(filtered_cyto2.shape[0])):
-    labels, details, _ = segment_with_diameter(
-        filtered_cyto2[slice],
-        model=model,
-        diameter=750,
-        z_axis=0,
-        channels=[1, 0],
-        min_diameter=200,  # default 200
-        diameter_step=200,  # default 200
-    )
-    output_dict["slice"].append(slice)
-    output_dict["labels"].append(labels)
-    output_dict["details"].append(details)
-
-del filtered_cyto2
-
-
-# In[ ]:
-
-
-organoid_masks = np.array(
-    list(
-        decouple_masks(
-            reverse_sliding_window_max_projection(
-                output_dict,
-                window_size=4,
-                original_z_slice_count=cyto2_image_shape[0],
-            ),
-            original_img_shape=cyto2_image_shape,
-            distance_threshold=40,
-        ).values()
-    )
-)
-
-
-# In[ ]:
-
-
-# generate the coordinates dataframe for reconstruction
-coordinates_df = generate_coordinates_for_reconstruction(organoid_masks)
-# generate distance pairs dataframe
-df = generate_distance_pairs(coordinates_df, x_y_vector_radius_max_constraint=20)
-# create and solve graph to get longest paths
-longest_paths = solve_graph(graph_creation(df))
-# collapse labels based on longest paths and reassign labels in organoid masks
-image = reassign_labels(organoid_masks, collapse_labels(coordinates_df, longest_paths))
-# refine the organoid masks
-organoid_mask = run_post_hoc_refinement(
-    mask_image=image,
-    sliding_window_context=3,
-)
-# clean up and send to gc
-del image, coordinates_df, df, longest_paths
 
 
 # ## Nuclei Segmentation
 
-# In[ ]:
+# In[6]:
 
 
 nuclei_image_shape = nuclei.shape
@@ -208,7 +123,7 @@ nuclei_masks = np.array(
 )
 
 
-# In[10]:
+# In[7]:
 
 
 # generate the coordinates dataframe for reconstruction
@@ -227,70 +142,40 @@ nuclei_mask = run_post_hoc_refinement(
 del image, coordinates_df, df, longest_paths
 
 
-# ## Segment the cells
-
-# In[11]:
+# In[8]:
 
 
-cell_mask = segment_cells_with_3D_watershed(
-    cyto_signal=cyto2,
-    nuclei_mask=nuclei_mask,
-)
+for zidx in range(nuclei_mask.shape[0]):
+    plt.subplot(121)
+    plt.imshow(nuclei[zidx, :, :], cmap="gray")
+    plt.axis("off")
+    plt.subplot(122)
+    plt.imshow(nuclei_mask[zidx, :, :], cmap="gray")
+    plt.axis("off")
+    plt.show()
 
 
 # ## run the mask reassignment function (post-hoc)
 # ### This needs to occur after both nuclei and cell segmentations are done
 
-# In[12]:
+# In[9]:
 
 
-cell_df = get_labels_for_post_hoc_reassignment(
-    compartment_mask=cell_mask, compartment_name="cell"
-)
 nuclei_df = get_labels_for_post_hoc_reassignment(
     compartment_mask=nuclei_mask, compartment_name="nuclei"
 )
 
 
-# In[13]:
-
-
-nuclei_mask, reassigned_nuclei_df = run_post_hoc_mask_reassignment(
-    nuclei_mask=nuclei_mask,
-    cell_mask=cell_mask,
-    nuclei_df=nuclei_df,
-    cell_df=cell_df,
-    return_dataframe=True,
-)
-
-
-# ## Cytoplasm Segmentation
-
-# In[14]:
-
-
-cytoplasm_mask = create_cytoplasm_masks(
-    nuclei_masks=nuclei_mask,
-    cell_masks=cell_mask,
-)
-
-
 # ## Save the segmented masks
 
-# In[ ]:
+# In[10]:
 
 
 nuclei_mask_output = pathlib.Path(f"{mask_path}/nuclei_mask.tiff")
-cell_mask_output = pathlib.Path(f"{mask_path}/cell_mask.tiff")
-cytoplasm_mask_output = pathlib.Path(f"{mask_path}/cytoplasm_mask.tiff")
-organoid_mask_output = pathlib.Path(f"{mask_path}/organoid_mask.tiff")
 tifffile.imwrite(nuclei_mask_output, nuclei_mask)
-tifffile.imwrite(cell_mask_output, cell_mask)
-tifffile.imwrite(cytoplasm_mask_output, cytoplasm_mask)
-tifffile.imwrite(organoid_mask_output, organoid_mask)
 
 
-# In[16]:
+# In[11]:
 
 
 end_mem = psutil.Process(os.getpid()).memory_info().rss / 1024**2
--- src/segmentation_utils/src/segmentation_utils.py
+++ src/segmentation_utils/src/segmentation_utils.py
@@ -88,36 +88,6 @@ def sliding_window_two_point_five_D(image_stack, window_size):
     return image_stack_2_5D
 
 
-# ----------------------------------------------------------------------
-# cellpose segementation
-# ----------------------------------------------------------------------
-def segmentaion_on_two_D(imgs):
-    use_GPU = torch.cuda.is_available()
-    # Load the model
-    model_name = "nuclei"
-
-    model = models.CellposeModel(
-        gpu=use_GPU,
-        model_type=model_name,
-    )
-    output_dict = {
-        "slice": [],
-        "labels": [],
-        "details": [],
-    }
-    for slice in tqdm.tqdm(range(imgs.shape[0])):
-        # Perform segmentation
-        output_dict["slice"].append(slice)
-        labels, details, _ = model.eval(
-            imgs[slice, :, :],
-            channels=[0, 0],
-            diameter=100,
-        )
-        output_dict["labels"].append(labels)
-        output_dict["details"].append(details)
-    return output_dict
-
-
 def reverse_sliding_window_max_projection(
     output_dict, window_size, original_z_slice_count
 ):
@@ -216,99 +186,6 @@ def apply_butterworth_filter(
     # add a guassian blur to the image
     img = skimage.filters.gaussian(img, sigma=1)
     return img
-
-
-def segment_with_diameter(
-    img: np.ndarray,
-    model: models.Cellpose,
-    diameter: int,
-    z_axis: int = 0,
-    channels: tuple = [1, 0],
-    min_diameter: int = 200,  # in pixels, default is 200
-    diameter_step: int = 200,
-) -> tuple:
-    """
-    Recursively perform segmentation, stepping down through diameters by 200
-    until a valid label is found or the minimum diameter is reached.
-    This effectively performs a dynamic search for the largest detectable object
-    in the image.
-
-    Parameters
-    ----------
-    img : np.ndarray
-        The image to segment. Can be 3D in the format of (z, y, x).
-    model : models.Cellpose
-        The Cellpose model to use for segmentation.
-    diameter : int
-        The diameter to use for segmentation.
-        This is where the search starts.
-    z_axis : int, optional
-        The axis of the z-stack. Default is axis 0.
-    channels : tuple, optional
-        The channels to use for segmentation. Default is (1, 0).
-        Where 1 is the channel for the cytoplasm and 0 using no other channel.
-    min_diameter : int, optional
-        The minimum diameter to use for segmentation.
-        If the diameter is less than this, the function will return empty labels.
-        Default is 200 pixels.
-    diameter_step : int, optional
-        The step size to decrease the diameter by when no labels are found.
-        Default is 200 pixels.
-
-    Returns
-    -------
-    tuple
-        labels : np.ndarray
-            The labels of the segmented image.
-        details : dict
-            The details of the segmentation.
-        _ : None
-            Placeholder for additional return values.
-    """
-
-    if diameter < min_diameter:
-        print("Minimum diameter reached. Returning empty labels.")
-        zero_labels = np.zeros_like(img)
-        return zero_labels, None, None
-
-    labels, details, _ = model.eval(
-        img, channels=channels, z_axis=z_axis, diameter=diameter
-    )
-
-    if labels is None:
-        print(f"Labels are empty for diameter {diameter}. Trying smaller diameter...")
-        return segment_with_diameter(
-            img, model, channels, z_axis, diameter - diameter_step
-        )
-
-    return labels, details, _
-
-
-# test the function with three cylinders each with a different diameter
-def test_segment_with_diameter():
-    diameters = [100, 250, 400, 600, 800, 1000]
-    z_depth = 1  # No z-depth for 2D images
-    for diameter in diameters:
-        img = np.zeros((1500, 1500), dtype=np.uint8)
-        rr, cc = skimage.draw.disk((500, 500), diameter / 2)
-        img[rr, cc] = 255
-
-        labels, details, _ = segment_with_diameter(
-            img,
-            model=models.CellposeModel(gpu=use_GPU, model_type="cyto3"),
-            diameter=diameter,
-            z_axis=z_depth,
-            channels=(1, 0),
-        )
-        # 100 should return empty labels
-        if diameter == 100:
-            assert np.all(labels == 0), (
-                f"Labels should be empty for diameter {diameter}"
-            )
-        else:
-            assert np.any(labels > 0), (
-                f"Labels should not be empty for diameter {diameter}"
-            )
 
 
 # ----------------------------------------------------------------------
