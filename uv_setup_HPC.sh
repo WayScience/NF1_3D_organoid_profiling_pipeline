d5573759a3ba7d7724c87a7941f89b5991a6b404
MikeLippincott <1michaell2017@gmail.com>
2026-03-16 14:59:45 -0600

    setup uv

--- uv_setup_HPC.sh
+++ uv_setup_HPC.sh
@@ -3,9 +3,9 @@
 # run twice to ensure we are in a clean environment
 # and not accidentally using an existing one
 # try but will not fail if not in a conda environment
-export SCRATCH_BASE="/scratch/$USER"
-export UV_CACHE_DIR="$SCRATCH_BASE/.cache/uv"
-export UV_PYTHON_INSTALL_DIR="$SCRATCH_BASE/.local/share/uv/python"
+export PROJECT_BASE="/projects/$USER/envs"
+export UV_CACHE_DIR="$PROJECT_BASE/.cache/uv"
+export UV_PYTHON_INSTALL_DIR="$PROJECT_BASE/nf1-3d-python"
 
 loops=2
 for _ in $(seq 1 $loops); do
@@ -20,17 +20,12 @@ unset VIRTUAL_ENV
 rm -f uv.lock
 rm -rf .venv
 
-uv venv /scratch/"$USER"/venvs/nf1-3d
+uv venv "$UV_PYTHON_INSTALL_DIR"
 
-# shellcheck disable=SC1090
-source /scratch/"$USER"/venvs/nf1-3d/bin/activate
+# shellcheck disable=SC1091
+source "$UV_PYTHON_INSTALL_DIR"/bin/activate
 uv sync --frozen
 
-# shellcheck disable=SC1091
-source .venv/bin/activate
-
-# shellcheck disable=SC1091
-source .venv/bin/activate
 # Use RELATIVE path - simple and reliable
 uv pip install -e ./utils
 uv pip install cosmicqc
