fb353621824bfd15bdc4e855636fcaf072e464f8
MikeLippincott <1michaell2017@gmail.com>
2026-03-26 12:46:59 -0600

    new uv s=config

--- 2.segment_images/child_segmentation.sh
+++ 2.segment_images/child_segmentation.sh
@@ -3,6 +3,7 @@
 git_root=$(git rev-parse --show-toplevel)
 
 if [ -d "/scratch/alpine" ]; then
+    echo "Using Alpine environment"
     ENV_PATH="/projects/mlippincott@xsede.org/software/uv/envs/nf1_uv_env/.venv"
 elif [ -d "/anvil" ]; then
     ENV_PATH="/anvil/projects/x-bio260064/software/uv/envs/nf1_uv_env/.venv"
@@ -10,6 +11,7 @@ else
     ENV_PATH="$git_root/.venv"
 fi
 
+PYTHON_BIN="$ENV_PATH/bin/python3"
 
 # shellcheck disable=SC1091
 source "$ENV_PATH"/bin/activate
@@ -28,21 +30,21 @@ log_file="$log_dir/segmentation_child_${patient}_${well_fov}.log"
 
 {
     echo "Processing well_fov $well_fov for patient $patient"
-    uv run python scripts/4.nuclei_segmentation.py \
+    "$PYTHON_BIN"  scripts/4.nuclei_segmentation.py \
         --patient "$patient" \
         --well_fov "$well_fov" \
         --input_subparent_name "$input_subparent_name" \
         --mask_subparent_name "$mask_subparent_name" \
         --clip_limit 0.02
 
-    uv run python scripts/5.cell_segmentation.py \
+    "$PYTHON_BIN"  scripts/5.cell_segmentation.py \
         --patient "$patient" \
         --well_fov "$well_fov" \
         --clip_limit 0.03 \
         --input_subparent_name "$input_subparent_name" \
         --mask_subparent_name "$mask_subparent_name"
 
-    uv run python scripts/6.organoid_segmentation.py \
+    "$PYTHON_BIN"  scripts/6.organoid_segmentation.py \
         --patient "$patient" \
         --well_fov "$well_fov" \
         --clip_limit 0.03 \
@@ -53,7 +55,7 @@ log_file="$log_dir/segmentation_child_${patient}_${well_fov}.log"
 
 } &> "$log_file"
 
-uv run python scripts/4.nuclei_segmentation.py \
+"$PYTHON_BIN"  scripts/4.nuclei_segmentation.py \
         --patient "NF0014_T1" \
         --well_fov "C4-2" \
         --input_subparent_name "zstack_images" \
