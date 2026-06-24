58857c88137417e46b83232f67a9a39d7b560984
MikeLippincott <1michaell2017@gmail.com>
2026-06-24 08:53:29 -0600

    remove the txt file

--- 3.cellprofiling/local_run_featurization_grand_parent.sh
+++ 3.cellprofiling/local_run_featurization_grand_parent.sh
@@ -46,7 +46,7 @@ while IFS= read -r line; do
         "$mask_subparent_name" \
         "$output_features_subparent_name"
 
-done < <(tac "$txt_file")
-# done < "$txt_file"
+# done < <(tac "$txt_file")
+done < "$txt_file"
 
 echo "Featurization done"
