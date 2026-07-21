0067528d8fc9c6413e2cc13d06fb77d3beb7f7e4
MikeLippincott <1michaell2017@gmail.com>
2026-07-21 11:25:49 -0600

    run all on HPC

--- 2.segment_images/parent_segmentation.sh
+++ 2.segment_images/parent_segmentation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --partition=amilan
+#SBATCH --partition=acpu
 #SBATCH --qos=cpu-long
 #SBATCH --account=amc-general
 #SBATCH --time=7-00:00
